d326270f1aff6f7f860901c6392fd5409f64e39c
q444172586 <18319042381@163.com>
2018-11-19 00:12:18 +0800

    添加quartz调度

---
+++ spring-utils/src/test/java/org/spring/utils/job/MyJob.java
@@ -0,0 +1,16 @@
+package org.spring.utils.job;
+
+import java.util.Date;
+
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+public class MyJob implements Job {
+
+	@Override
+	public void execute(JobExecutionContext arg0) throws JobExecutionException {
+		System.out.println(new Date() + ": doing something...");
+	}	
+
+}
